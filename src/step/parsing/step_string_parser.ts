9b6b4df08101c5f4bd059b699e329b3774309f4d
Conor Stokes <conor.stokes@burningcandle.io>
2025-02-14 05:37:15 +0800

    Test and lint fix

--- src/step/parsing/step_string_parser.ts
+++ src/step/parsing/step_string_parser.ts
@@ -231,11 +231,11 @@ export default class StepStringParser extends ParsingDfa16Table {
               if (nextChar3 < 0x7F) {
                 result += String.fromCharCode(nextChar3)
               } else
-                if (nextChar >= 0xA1 && nextChar <= 0xFF) {
-                  result += ISO8859Table[codePage][nextChar - 0xA1]
-                } else {
-                  return result
-                }
+              if (nextChar >= 0xA1 && nextChar <= 0xFF) {
+                result += ISO8859Table[codePage][nextChar - 0xA1]
+              } else {
+                return result
+              }
               /* eslint-enable no-magic-numbers */
 
               cursor = nextCursor3 + 1
